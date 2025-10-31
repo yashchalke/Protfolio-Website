4ede75ac651fc45294ab6fcf5d62cf25e5f73133
yashchalke <chalkeyashkrishna@gmail.com>
2025-10-31 17:40:28 +0530

    Built the app for the production

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,15 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
+  build: {
+  chunkSizeWarningLimit: 1000,
+  rollupOptions: {
+    output: {
+      manualChunks: {
+        react: ['react', 'react-dom'],
+        ui: ['lucide-react'],
+      }
+    }
+  }
+}
 })
